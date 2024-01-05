98414b9b88a23ee1903360c75eee2729d1965c86
Nora <nora.ekramy11@gmail.com>
2024-01-06 01:33:37 +0200

    Heap - Sort

--- binary_trees.h
+++ binary_trees.h
@@ -72,5 +72,6 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
-
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 #endif /* _BINARY_TREES_H_ */
